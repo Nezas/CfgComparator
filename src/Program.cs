cf342a06b5485af91517d42f14a2fb89874af20e
Nezas <nedas.zaksauskas@gmail.com>
2021-09-20 12:57:07 +0300

    Added user input for file names

--- src/Program.cs
+++ src/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
 using CfgComparator.Models;
 using CfgComparator.Writers;
 
@@ -8,20 +10,32 @@ namespace CfgComparator
     {
         static void Main(string[] args)
         {
-            Record source = Reader.Read("../../../CfgData/FMB001-default.cfg");
-            Record target = Reader.Read("../../../CfgData/FMB920-default.cfg");
-
             Comparator comparator = new();
-            comparator.Compare(source, target);
-
-            List<string> unchanged = comparator.Unchanged;
-            List<string> modified = comparator.Modified;
-            List<string> removed = comparator.Removed;
-            List<string> added= comparator.Added;
-
             Output output = new(new ConsoleWriter());
-            Menu menu = new(source, target, unchanged, modified, removed, added, output);
-            menu.Start();
+
+            try
+            {
+                Console.Write("Enter SOURCE configuration file name (with .cfg): ");
+                string sourceFile = Console.ReadLine();
+                Record source = Reader.Read($"../../../CfgData/{sourceFile}");
+                Console.Write("Enter TARGET configuration file name (with .cfg): ");
+                string targetFile = Console.ReadLine();
+                Record target = Reader.Read($"../../../CfgData/{targetFile}");
+                comparator.Compare(source, target);
+
+                List<string> unchanged = comparator.Unchanged;
+                List<string> modified = comparator.Modified;
+                List<string> removed = comparator.Removed;
+                List<string> added = comparator.Added;
+
+                Menu menu = new(source, target, unchanged, modified, removed, added, output);
+                menu.Start();
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
         }
     }
 }
